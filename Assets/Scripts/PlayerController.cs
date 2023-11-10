7d0ebb87adddd2a065c56ab8b760cd1804ec791e
Skymaslo <wsop.top.chips@mail.ru>
2023-11-10 19:07:18 +0300

    mASLO GAME

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,22 +8,28 @@ public class PlayerController : MonoBehaviour
     public float speed = 1;
     public float mouseSensative = 1;
     public float jumpStrength = 5;
+    public GameObject GroundCheaker;
+    public float GroundCheckRadius;
+    public LayerMask ignoreLayer;
 
     private Vector3 playerVelocity;
     private float gravityValue = -9.8f;
+    private Animator animator;
 
     private CharacterController characterController;
     // Start is called before the first frame update
     void Start()
     {
-        characterController = GetComponent<CharacterController>(); 
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        bool grounded = characterController.isGrounded;
+        //bool grounded = characterController.isGrounded;
+        bool grounded = IsGrounded();
 
         if (grounded && playerVelocity.y < 0)
         {
@@ -34,6 +40,11 @@ public class PlayerController : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         float moveY = Input.GetAxis("Vertical");
         Vector3 moveVector = Camera.main.transform.forward * moveY + Camera.main.transform.right * moveX;
+        moveVector.y = 0;
+
+        float moveSpeed = moveVector.sqrMagnitude;
+        animator.SetFloat("Speed", moveSpeed);
+
         characterController.Move(moveVector * speed * Time.deltaTime);
         
 
@@ -43,6 +54,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             playerVelocity.y += Mathf.Sqrt(jumpStrength * -3 * gravityValue);
+            animator.SetTrigger("jump");
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
@@ -50,5 +62,25 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    private void OnDrawGizmos()
+    {
+        Color32 color = Color.green;
+        color.a = 128;
+        Gizmos.color = color;
 
+        Gizmos.DrawSphere(GroundCheaker.transform.position, GroundCheckRadius);
+    }
+    public bool IsGrounded()
+    {
+        Collider[] colliders = Physics.OverlapSphere(GroundCheaker.transform.position, GroundCheckRadius, ignoreLayer);
+        if(colliders.Length > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
 }
